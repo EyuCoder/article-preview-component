0d01e1322d3573f2073b9edfa0499f94e8d30153
venom <eyueldaniel0921@gmail.com>
2022-12-02 21:22:20 +0300

    font and layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['Manrope', 'sans'],
+    },
+    extend: {
+      colors: {
+        blue: {
+          dark: '#6d7f97',
+        },
+        gray: {
+          'grayish-blue': '#9eafc2',
+          'dark-grayish-blue': '#48556a',
+          'light-grayish-blue': '#ecf2f8',
+        }
+      },
+    },
   },
   plugins: [],
 }
